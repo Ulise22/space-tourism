27cbab67796722c00ab108b5f06e026c0524fab3
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-04-27 21:16:06 -0300

    DestinationPage almost done

--- pages/destination/index.tsx
+++ pages/destination/index.tsx
@@ -1,11 +1,48 @@
 import LayoutPage from "@/components/LayoutPage";
 import styles from './Destination.module.css'
+import { useState } from "react";
+import {pictures} from './pictures';
+import Image from "next/image";
+import data from '../../components/data.json';
 
 export default function Destination () {
-    return(
-        <LayoutPage title="- Destination">
-            <main className={styles.destination}>
+    const destinations = data.destinations;
+    const [desIndex, setDesIndex] = useState(0);
 
+    return(
+        <LayoutPage title="- Destinations">
+            <main className={styles.destination}>
+                <div className={styles.destination__container}>
+                    <section className={styles.destination__visual}>
+                        <h2 className={styles.destination__visual__title}> <span>01</span> PICK YOUR DESTINATION </h2>
+
+                        <Image className={styles.destination__visual__image} src={pictures[desIndex]} alt="planet" />
+                    </section>
+
+                    <section className={styles.destination__info}>
+                        <ul className={styles.destination__info__list}>
+                            <li className={styles.destination__info__list__item} onClick={() => setDesIndex(0)} >MOON</li>
+                            <li className={styles.destination__info__list__item} onClick={() => setDesIndex(1)} >MARS</li>
+                            <li className={styles.destination__info__list__item} onClick={() => setDesIndex(2)} >EUROPA</li>
+                            <li className={styles.destination__info__list__item} onClick={() => setDesIndex(3)} >TITAN</li>
+                        </ul>
+                        <h1 className={styles.destination__info__title}> {destinations[desIndex].name} </h1>
+                        <p className={styles.destination__info__text}> {destinations[desIndex].description} </p>
+
+                        <span className={styles.destination__line}></span>
+
+                        <article className={styles.destination__info__article}>
+                            <div className={styles.destination__info__article__container}>
+                                <p className={styles.destination__info__article__p}>AVG. DISTANCE</p>
+                                <h4 className={styles.destination__info__article__stats}> {destinations[desIndex].distance} </h4>
+                            </div>
+                            <div className={styles.destination__info__article__container}>
+                                <p className={styles.destination__info__article__p}>EST. TRAVEL TIME</p>
+                                <h4 className={styles.destination__info__article__stats}> {destinations[desIndex].travel} </h4>
+                            </div>
+                        </article>
+                    </section>
+                </div>
             </main>
         </LayoutPage>
     )
---
+++ pages/destination/pictures.js
@@ -0,0 +1,6 @@
+import moon from './images/image-moon.png';
+import mars from './images/image-mars.png';
+import europa from './images/image-europa.png';
+import titan from './images/image-titan.png';
+
+export const pictures = [moon, mars, europa, titan]
