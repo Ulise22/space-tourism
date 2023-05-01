da3746c37a9777143f3febc87cca8ad718ba1317
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-05-01 13:20:30 -0300

    list in destinationPage fixed

--- pages/technology/index.tsx
+++ pages/technology/index.tsx
@@ -1,11 +1,14 @@
 import LayoutPage from "@/components/LayoutPage";
 import styles from './Technology.module.css'
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Technology () {
+    const [tecIndex, setTecIndex] = useState(2)
     return(
         <LayoutPage title="- Technology">
             <main className={styles.technology}>
-
+                <div className={tecIndex == 0 ? `${styles.firstImage}` : tecIndex == 1 ? `${styles.secondImage}` : `${styles.thirdImage}`}></div>
             </main>
         </LayoutPage>
     )
